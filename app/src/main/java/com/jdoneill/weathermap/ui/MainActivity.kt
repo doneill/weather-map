1424f43837688deb22c1a926f988bd19b8f6894b
Dan O'Neill <doneill@esri.com>
2017-11-03 12:53:47 -0700

    implement view

--- app/src/main/java/com/jdoneill/weathermap/ui/MainActivity.kt
+++ app/src/main/java/com/jdoneill/weathermap/ui/MainActivity.kt
@@ -17,19 +17,35 @@
 
 package com.jdoneill.weathermap.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.MotionEvent
+import android.widget.TextView
+import com.esri.arcgisruntime.geometry.GeometryEngine
+import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.geometry.SpatialReferences
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.view.*
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
 import com.jdoneill.weathermap.R
+import com.jdoneill.weathermap.data.Weather
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    // degree sign
+    private val DEGREE: String = "\u00B0"
+    private lateinit var callout: Callout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +55,47 @@ class MainActivity : AppCompatActivity() {
         // show map
         val map = ArcGISMap(Basemap.Type.DARK_GRAY_CANVAS_VECTOR, 47.495052, -121.786863, 12)
         mapView.map = map
+        // graphics overlay for tapped location marker
+        val mvOverlay = addGraphicsOverlay(mapView)
+
+        // respond to single taps on mapview
+        mapView.onTouchListener = object: DefaultMapViewOnTouchListener(this, mapView) {
+            override fun onSingleTapConfirmed(motionEvent: MotionEvent?): Boolean {
+                // clear any graphics and callouts
+                mvOverlay.graphics.clear()
+                mapView.callout.dismiss()
+                // get the point that was clicked and convert it to a point in map coordinates
+                val screenPoint: android.graphics.Point = android.graphics.Point(motionEvent!!.x.toInt(), motionEvent.y.toInt())
+                // create a map point from screen point
+                val mapPoint: Point = mapView.screenToLocation(screenPoint)
+                // convert to WGS84 for lat/lon format
+                val wgs84: Point = GeometryEngine.project(mapPoint, SpatialReferences.getWgs84()) as Point
+
+                // get weather from location tapped
+                val network = WeatherClient()
+                val call = network.getWeatherForCoord(wgs84.y.toFloat(), wgs84.x.toFloat())
+                call.enqueue(object : Callback<Weather> {
+                    override fun onResponse(call: Call<Weather>?, response: Response<Weather>?) {
+                        val weather: Weather? = response?.body()
+                        // present date if main not null
+                        weather?.let { presentData(it, mapPoint, mvOverlay) }
+
+                        val name = weather?.name
+                        val sys = weather?.sys
+                        val sunrise = sys?.sunrise
+                        val sunset = sys?.sunset
+                        println("station name: $name | sunrise: $sunrise | sunset: $sunset")
+
+                    }
+
+                    override fun onFailure(call: Call<Weather>?, t: Throwable?) {
+                        t?.printStackTrace()
+                    }
+                })
+                return super.onSingleTapConfirmed(motionEvent)
+            }
+
+        }
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -76,5 +133,50 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         mapView.resume()
+    }
+
+    /**
+     * Create a Graphics Overlay
+     *
+     * @param mapView MapView to add the graphics overlay to
+     */
+    private fun addGraphicsOverlay(mapView: MapView) : GraphicsOverlay {
+        // create graphics overlay
+        val graphicsOverlay = GraphicsOverlay()
+        // add overlay to MapView
+        mapView.graphicsOverlays.add(graphicsOverlay)
+        return graphicsOverlay
+    }
+
+    /**
+     * Present the weather data in a Callout
+     *
+     * @param weather weather data
+     * @param mapPoint location to show Callout
+     * @param dataOverlay GraphicsOverlay to add Marker
+     */
+    private fun presentData(weather: Weather, mapPoint: Point, dataOverlay: GraphicsOverlay) = with(weather){
+        val cityName = name
+        val temp = main.temp
+        val highTemp = main.minTemp
+        val lowTemp = main.maxTemp
+
+        // create a marker at tapped location
+        val locationMarker = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CROSS, Color.WHITE, 15.0f)
+        val locationGraphic = Graphic(mapPoint, locationMarker)
+        dataOverlay.graphics.add(locationGraphic)
+
+        // create a textview for the callout
+        val calloutContent = TextView(applicationContext)
+        calloutContent.setTextColor(Color.BLACK)
+        // format coordinates to 4 decimal places
+        calloutContent.text = "Name: $cityName | Temp: $temp$DEGREE | High: $highTemp$DEGREE | Low: $lowTemp$DEGREE"
+        // get callout, set content and geoelement graphic
+        callout = mapView.callout
+        callout.content = calloutContent
+        callout.setGeoElement(locationGraphic, mapPoint)
+        callout.show()
+        // center on the tapped location
+        mapView.setViewpointCenterAsync(mapPoint)
     }
 }
