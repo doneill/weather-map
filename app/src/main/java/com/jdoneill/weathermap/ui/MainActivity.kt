5385013cec9659abc2b48b0cf358b473a22348a1
Dan O'Neill <dev@jdoneill.com>
2019-12-14 11:01:27 -0800

    update colors and basemap

--- app/src/main/java/com/jdoneill/weathermap/ui/MainActivity.kt
+++ app/src/main/java/com/jdoneill/weathermap/ui/MainActivity.kt
@@ -67,7 +67,7 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         val extras = intent.extras
 
         // show mMap
-        mMap = ArcGISMap(Basemap.createDarkGrayCanvasVector())
+        mMap = ArcGISMap(Basemap.createStreetsVector())
         mapView.map = mMap
 
         // graphics overlay for tapped location marker
