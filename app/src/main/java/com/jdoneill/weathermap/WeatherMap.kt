c73b855cfb1dd89945ea2a64bc6d06db9ec7faed
Dan O'Neill <dev@jdoneill.com>
2020-05-30 11:21:56 -0700

    init koin

--- app/src/main/java/com/jdoneill/weathermap/WeatherMap.kt
+++ app/src/main/java/com/jdoneill/weathermap/WeatherMap.kt
@@ -3,6 +3,8 @@ package com.jdoneill.weathermap
 import android.app.Application
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 import timber.log.Timber
 import timber.log.Timber.DebugTree
@@ -14,6 +16,10 @@ class WeatherMap : Application() {
     override fun onCreate() {
         super.onCreate()
         setupLeakCanary()
+
+        startKoin {
+            androidContext(this@WeatherMap)
+        }
 
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
