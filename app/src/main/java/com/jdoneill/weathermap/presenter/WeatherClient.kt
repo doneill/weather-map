280241433aaa608e605ba80bb8d66a205f4c226b
Dan O'Neill <dev@jdoneill.com>
2021-06-13 19:01:54 -0700

    weather api custom logger

--- app/src/main/java/com/jdoneill/weathermap/presenter/WeatherClient.kt
+++ app/src/main/java/com/jdoneill/weathermap/presenter/WeatherClient.kt
@@ -1,9 +1,14 @@
 package com.jdoneill.weathermap.presenter
 
+import com.google.gson.GsonBuilder
+
 import com.jdoneill.weathermap.BuildConfig
-import com.jdoneill.weathermap.data.Constants
 import com.jdoneill.weathermap.data.WeatherData
 import com.jdoneill.weathermap.service.WeatherService
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -17,9 +22,20 @@ class WeatherClient {
      * @lon coordinate longitude
      */
     suspend fun getWeatherForCoord(lat: Float, lon: Float): WeatherData {
+        val loggingInterceptor = HttpLoggingInterceptor()
+
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.HEADERS
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+
         val network = Retrofit.Builder()
-                .baseUrl(Constants.HTTP_API_OPENWEATHERMAP_URL)
-                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
                 .build()
         val weatherService = network.create(WeatherService::class.java)
 
